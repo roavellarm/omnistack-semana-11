466b2fb994ec7e26c7103adb13d6efeb2457cc5f
Rodrigo Avellar <roavellarm@gmail.com>
2020-05-20 01:30:09 -0300

    Updates and remove node modules

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,7 +3,25 @@ const crypto = require("crypto");
 
 module.exports = {
   async index(request, response) {
-    const incidents = await connection("incidents").select("*");
+    const { page = 1 } = request.query;
+
+    const [count] = await connection("incidents").count();
+
+    const incidents = await connection("incidents")
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        "incidents.*",
+        "ongs.name",
+        "ongs.email",
+        "ongs.whatsapp",
+        "ongs.city",
+        "ongs.uf",
+      ]);
+
+    response.header("X-Total-Count", count["count(*)"]);
+
     return response.json(incidents);
   },
 
