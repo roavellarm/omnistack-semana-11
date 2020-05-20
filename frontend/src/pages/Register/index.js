3a4e1ea74489a623ba64f861d1b20c173eaad1c5
Rodrigo Avellar <roavellarm@gmail.com>
2020-05-20 20:29:43 -0300

    Remove import

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import api from "../../services/api";
 import { FiArrowLeft } from "react-icons/fi";
