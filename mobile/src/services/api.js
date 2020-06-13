29d1455d910c937f2b55953aca0a8118f8d95982
Rodrigo Avellar <roavellarm@gmail.com>
2020-06-13 16:56:22 -0300

    Update api

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // baseURL: 'http://g2-s2d.anonymous.mobile.exp.direct:3333',
-  baseURL: 'http://192.168.0.191:3333/',
+  baseURL: 'https://apibth.herokuapp.com/',
 })
 
 export default api
