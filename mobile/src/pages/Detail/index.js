cc9dcc00e1bac9c6d19a01ff73a775968595aab3
Rodrigo Avellar <roavellarm@gmail.com>
2020-06-13 16:56:01 -0300

    Update detail

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Feather } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { View, Text, Image, TouchableOpacity, Linking } from 'react-native'
 import * as MailComposer from 'expo-mail-composer'
 import logoImg from '../../assets/logo.png'
@@ -8,8 +8,17 @@ import styles from './styles'
 
 export default function Detail() {
   const navigation = useNavigation()
-  const message =
-    'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$120,00'
+  const route = useRoute()
+
+  const incident = route.params.incident
+  const message = `Olá ${
+    incident.name
+  }, estou entrando em contato pois gostaria de ajudar no caso "${
+    incident.title
+  }" com o valor de ${Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(incident.value)}`
 
   function navigateBack() {
     navigation.goBack()
@@ -17,8 +26,8 @@ export default function Detail() {
 
   function sendMail() {
     MailComposer.composeAsync({
-      subject: `Herói do Caso: Cadelinha atropelada`,
-      recipients: ['ramprofissional@gmail.com'],
+      subject: `Herói do Caso: ${incident.title}`,
+      recipients: [incident.email],
       body: message,
     })
   }
@@ -29,7 +38,9 @@ export default function Detail() {
     Todo aplicativo (pelo menos os mais famosos) em celulares tem o deep linking.
   */
   function sendWhatsapp() {
-    Linking.openURL(`whatsapp://send?phone=+5551981937660&text=${message}`)
+    Linking.openURL(
+      `whatsapp://send?phone=${incident.whatsapp}&text=${message}`
+    )
   }
 
   return (
@@ -43,13 +54,20 @@ export default function Detail() {
 
       <View style={styles.incident}>
         <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-        <Text style={styles.incidentValue}>APAD</Text>
+        <Text style={styles.incidentValue}>
+          {incident.name} de {incident.city}/{incident.uf}
+        </Text>
 
         <Text style={styles.incidentProperty}>CASO:</Text>
-        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+        <Text style={styles.incidentValue}>{incident.title}</Text>
 
         <Text style={styles.incidentProperty}>VALOR:</Text>
-        <Text style={styles.incidentValue}>R$ 120,00</Text>
+        <Text style={styles.incidentValue}>
+          {Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(incident.value)}
+        </Text>
       </View>
 
       <View style={styles.contactBox}>
