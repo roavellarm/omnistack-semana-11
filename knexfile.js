a7bc50a08ef34ba322f679b8b3307d91aa362faf
Rodrigo Avellar <roavellarm@gmail.com>
2020-06-14 23:42:05 -0300

    change knexfile

--- knexfile.js
+++ knexfile.js
@@ -49,7 +49,8 @@ module.exports = {
       max: 10,
     },
     migrations: {
-      directory: `${__dirname}/dist/database/migrations`,
+      // directory: `${__dirname}/dist/database/migrations`,
+      tableName: 'knex_migrations',
     },
   },
 
